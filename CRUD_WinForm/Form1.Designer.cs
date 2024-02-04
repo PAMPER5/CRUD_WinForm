ffbc0fdf9012f975d08d1e08f35a31a3fa4b9384
PAMPER5 <uglanovmurad1999@gmail.com>
2024-02-04 18:05:49 +0300

    Реализовать метод добавления записей в БД

--- CRUD_WinForm/Form1.Designer.cs
+++ CRUD_WinForm/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelMark = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxMark = new System.Windows.Forms.TextBox();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(118, 47);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(44, 16);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // labelMark
+            // 
+            this.labelMark.AutoSize = true;
+            this.labelMark.Location = new System.Drawing.Point(121, 125);
+            this.labelMark.Name = "labelMark";
+            this.labelMark.Size = new System.Drawing.Size(37, 16);
+            this.labelMark.TabIndex = 1;
+            this.labelMark.Text = "Mark";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(196, 47);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(205, 22);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxMark
+            // 
+            this.textBoxMark.Location = new System.Drawing.Point(196, 118);
+            this.textBoxMark.Name = "textBoxMark";
+            this.textBoxMark.Size = new System.Drawing.Size(205, 22);
+            this.textBoxMark.TabIndex = 3;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(166, 217);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 4;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.textBoxMark);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelMark);
+            this.Controls.Add(this.labelName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelMark;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxMark;
+        private System.Windows.Forms.Button buttonInsert;
     }
 }
 
--- CRUD_WinForm/Form1.cs
+++ CRUD_WinForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CRUD_WinForm
 {
@@ -15,6 +16,21 @@ namespace CRUD_WinForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            string strConnection = "data source=murzilca;initial catalog=WinFormCRUD;Integrated Security=True;";
+            SqlConnection connection = new SqlConnection(strConnection);
+            string sqlString = "insert into tblStudent values ('"+textBoxName.Text+"','"+textBoxMark.Text+"')";
+            SqlCommand command = new SqlCommand(sqlString, connection);
+            connection.Open();
+            int n = command.ExecuteNonQuery();
+            if (n == 1)
+                MessageBox.Show("Successfull");
+            else
+                MessageBox.Show("Fail");
+            connection.Close();
         }
     }
 }
